88deb45d206ef4751b3dfe488bd21402ccbcbf68
zhizhou.ren <zhizhou.ren@changhong.com>
2018-09-20 17:37:35 +0800

    bug fixed

--- server/src/main/java/org/schemarepo/config/Bootstrap.java
+++ server/src/main/java/org/schemarepo/config/Bootstrap.java
@@ -4,7 +4,7 @@ import com.google.inject.Singleton;
 import com.sun.jersey.spi.container.servlet.ServletContainer;
 import com.sun.jersey.spi.container.servlet.WebConfig;
 
-import io.swagger.jaxrs.config.BeanConfig;
+//import io.swagger.jaxrs.config.BeanConfig;
 import javax.servlet.ServletException;
 
 
@@ -13,12 +13,12 @@ public class Bootstrap extends ServletContainer {
   @Override
   protected void init(WebConfig config) throws ServletException {
     super.init(config);
-    BeanConfig beanConfig = new BeanConfig();
-    beanConfig.setVersion("1.0.0");
-    beanConfig.setSchemes(new String[]{"http"});
-    beanConfig.setHost("localhost:8085");
-    beanConfig.setBasePath("/api");
-    beanConfig.setResourcePackage("org.schemarepo.rest");
-    beanConfig.setScan(true);
+//    BeanConfig beanConfig = new BeanConfig();
+//    beanConfig.setVersion("1.0.0");
+//    beanConfig.setSchemes(new String[]{"http"});
+//    beanConfig.setHost("localhost:8085");
+//    beanConfig.setBasePath("/api");
+//    beanConfig.setResourcePackage("org.schemarepo.rest");
+//    beanConfig.setScan(true);
   }
 }
