8fdd8befe6f99c35489b3da3683fc5f604f497c5
zhizhou.ren <zhizhou.ren@changhong.com>
2018-09-21 14:31:04 +0800

    bug fixed

--- server/src/main/java/org/schemarepo/config/CustomServerConnector.java
+++ server/src/main/java/org/schemarepo/config/CustomServerConnector.java
@@ -19,37 +19,30 @@ import com.google.inject.Singleton;
 @Singleton
 public class CustomServerConnector extends ServerConnector {
 
-  @Inject
   public CustomServerConnector(Server server) {
     super(server);
   }
 
-  @Inject
   public CustomServerConnector(Server server, int acceptors, int selectors) {
     super(server, acceptors, selectors);
   }
 
-  @Inject
   public CustomServerConnector(Server server, int acceptors, int selectors, ConnectionFactory... factories) {
     super(server, acceptors, selectors, factories);
   }
 
-  @Inject
   public CustomServerConnector(Server server, ConnectionFactory... factories) {
     super(server, factories);
   }
 
-  @Inject
   public CustomServerConnector(Server server, SslContextFactory sslContextFactory) {
     super(server, sslContextFactory);
   }
 
-  @Inject
   public CustomServerConnector(Server server, int acceptors, int selectors, SslContextFactory sslContextFactory) {
     super(server, acceptors, selectors, sslContextFactory);
   }
 
-  @Inject
   public CustomServerConnector(Server server, SslContextFactory sslContextFactory, ConnectionFactory... factories) {
     super(server, sslContextFactory, factories);
   }
